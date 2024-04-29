5ec291fe6a9c2d6924430867eac7b61e6d0d5218
vladloot <vlad.agito@gmail.com>
2024-04-29 15:38:25 +0300

    create checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { CheckBox } from '@/components/ui/checkBox/CheckBox'
 import { TabSwitcher } from '@/components/ui/tabs/Tabs'
 
 export function App() {
   return (
     <div>
       <TabSwitcher />
+      <CheckBox disabled label={'checkbox'} />
+      <CheckBox label={'checkbox'} />
     </div>
   )
 }
